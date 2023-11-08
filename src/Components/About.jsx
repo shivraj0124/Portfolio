88bd5a94a15a6265dbd1b8ecd723810baa6008c9
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-11-09 03:47:30 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ function About() {
                 <div className='w-[100%] flex flex-col justify-start items-start'>
                     <h1 className='font-bold text-3xl'>About Me</h1>
                     <h1 className='text-2xl  font-semibold text-cyan-700'>Web Developer</h1>
-                    <p>Hello !!! I am Shivraj Santosh Kolwankar.Currently pursuing diploma in Information Technology from Government Polytechnic Mumbai.I am very mush interested in programming and web development.Enthusiastic to improve my knowledge and learn new Technolgies.Programming Language and Frameworks known: Javascript React js Node js Express js Python C C++ Java</p>
+                    <p>Hello !!! I am Shivraj Santosh Kolwankar.Currently pursuing diploma in Information Technology from Government Polytechnic Mumbai.I am very much interested in programming and web development.Enthusiastic to improve my knowledge and learn new Technolgies.Programming Language and Frameworks known: Javascript React js Node js Express js Python C C++ Java</p>
                     <div className='text-sm mt-5 flex flex-row gap-4 '>
                         <button className='py-1 px-3 hover:bg-cyan-500 hover:text-white border-2 border-cyan-500 text-cyan-600 font-semibold rounded-md'>Download CV</button>
                         
