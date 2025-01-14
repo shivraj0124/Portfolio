95239b4a7d1539ec942967c7e0dbf891468b06cd
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2025-01-14 15:10:38 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import img1 from "../assets/home-image.png";
-import resume from "../assets/Resume_Shivraj_Kolwankar.pdf";
+import resume from "../assets/Shivraj_Kolwankar_Web_Developer_Resume.pdf";
 import { saveAs } from "file-saver";
 import themeHook from "./ThemeContext";
 function About() {
